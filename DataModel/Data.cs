5863498763313f8d91d08c7ddaeebdd1c67db684
ezge <ezge.rea@gmail.com>
2018-03-06 23:40:01 +0100

    initial commit

---
+++ DataModel/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+
+        public static List<Data> GetAllData()
+        {
+            List<Data> listData = new List<Data>
+            {
+                new Data
+                { Id = 1, Name = "AAA", Qty = 10, Price = 5},
+                
+                new Data
+                { Id = 2, Name = "AAA", Qty = 5, Price = 6}
+            };
+
+            return listData;
+        }
+    }
+}
---
+++ LinqDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Data.GetAllData().GroupBy(x => new { x.Name })
+                                          .Select(x => new
+                                                        {
+                                                            Id = 0,
+                                                            Name = x.Key.Name,
+                                                            Sum = x.Sum(y => y.Qty),
+                                                            Avg = x.Average(z => z.Price)
+                                                        });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("{{{0}, \"{1}\", {2}, {3:0.##}}}", item.Id, item.Name, item.Sum, item.Avg);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
